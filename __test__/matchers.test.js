94967c59584106c5f963554a7566f3156ec9e2af
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-06-09 13:13:28 +0200

    test: commons matchers

---
+++ __test__/matchers.test.js
@@ -0,0 +1,18 @@
+describe("Common matchers", () => {
+  const user = {
+    name: "Adri",
+    lastName: "Antanon",
+  };
+  const user2 = {
+    name: "Adri",
+    lastName: "Antanon",
+  };
+
+  test("should be equal each other", () => {
+    expect(user).toEqual(user2);
+  });
+
+  test("shouldn't be equal each other", () => {
+    expect(user).not.toEqual(user2);
+  });
+});

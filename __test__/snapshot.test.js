56f1738ec664b93163fc448a650cc5aca364a65f
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-06-14 09:55:24 +0200

    test: :white_check_mark: snapshots exceptions

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,4 +5,24 @@ describe("It's time for the snapshots", () => {
   test("Snapshot ", () => {
     expect(getCharacters(rick)).toMatchSnapshot();
   });
+
+  test("should fail the snapshot", () => {
+    const user = {
+      createAt: new Date(),
+      id: Math.floor(Math.random() * 20),
+    };
+
+    expect(user).toMatchSnapshot();
+  });
+
+  test("should have an exception", () => {
+    const user = {
+      name: "Adri",
+      id: Math.floor(Math.random() * 20),
+    };
+
+    expect(user).toMatchSnapshot({
+      id: expect.any(Number),
+    });
+  });
 });
